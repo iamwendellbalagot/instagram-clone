168e9fa6191f649b5a072bab5a92fa2761d9e2a9
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-11-02 05:04:15 +0800

    Instance

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+import Avatar from '@material-ui/core/Avatar';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img 
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+                alt='Logo'/>
+            <div className='header__user'>
+                <button>Logout</button>
+                <Avatar />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+import Header from '../../components/Header/Header';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
+
